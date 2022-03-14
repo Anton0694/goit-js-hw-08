8397bc3347c2f1a6ba514cd0d49d872a0012d82e
Anton Sushchenko <vailant0694@gmail.com>
2022-03-14 14:40:51 +0200

    master

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,26 @@ import { galleryItems } from './gallery-items';
 
 
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
+function renderingGallery(array) {
+  const gallery = array
+    .map(
+      (image) =>
+            `
+        <a class = "gallery__item" href="${image.original}">
+        <img class = "gallery__image" src = "${image.preview}"
+        alt = "${image.description}"></a>
+        `
+    )
+    .join("");
+
+  galleryRef.insertAdjacentHTML("beforeend", gallery);
+}
+
+renderingGallery(galleryItems);
+let gallery = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
